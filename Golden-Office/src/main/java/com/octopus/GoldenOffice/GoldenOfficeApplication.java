28a749b099f60960b2b79bc08ef6f33b4899c3c7
Saumya999 <saumyadiptasarkar49@gmail.com>
2020-08-14 12:44:46 +0530

    Maven Boiler Plate

---
+++ Golden-Office/src/main/java/com/octopus/GoldenOffice/GoldenOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.octopus.GoldenOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenOfficeApplication.class, args);
+	}
+
+}
---
+++ Golden-Office/src/test/java/com/octopus/GoldenOffice/GoldenOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octopus.GoldenOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
